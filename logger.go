fb26c36433a9fc326e3611c490a1f2dc6a07faa6
Ahmed Abdraboh <ahmed.elsayed.abdraboh@gmail.com>
2019-01-30 15:03:44 +0200

    new logggg

--- logger.go
+++ logger.go
@@ -3,5 +3,5 @@ package go_logger
 import "fmt"
 
 func Log(logInfo string)  {
-	fmt.Println("Thanks for using the new Logger" + logInfo)
+	fmt.Println("Thanks for using the new Logger   ->  " + logInfo)
 }
